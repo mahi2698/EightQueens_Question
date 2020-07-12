3c5fade01c7f4e94ee3f78c9e508f96a97c8841b
hp <hp@DESKTOP-FBSITBI>
2020-07-12 13:16:55 +0530

    Completed the required logic

--- EightQueens_Question/src/ChessBoardRenderer.java
+++ EightQueens_Question/src/ChessBoardRenderer.java
@@ -2,7 +2,15 @@ public class ChessBoardRenderer {
 	public boolean isBlackSquare(int square) {
 
 		// WRITE YOUR LOGIC HERE...
-		System.out.println(square);
-		return true;
+		int row=(square/8);
+		int column=(square%8);
+		if(row%2==0&&column%2==0)
+			return false;
+		else if(row%2==0&&column%2!=0)
+			return true;
+		else if(row%2!=0&&column%2==0)
+			return true;
+		else
+			return false;
 	}
 }
--- EightQueens_Question/src/GameStrategy.java
+++ EightQueens_Question/src/GameStrategy.java
@@ -4,19 +4,18 @@ public class GameStrategy {
 
 	private int getColumn(int cellId) {
 		// WRITE YOUR LOGIC HERE...................................		
-
-		return 0;	// just for the heck of it
+		int column=(cellId%8);
+		return column;	
 	}
 	
 	private int getRow(int cellId) {
 		// WRITE YOUR LOGIC HERE....................................
-		
-		return 0;	// just for the heck of it
+		int row=(cellId/8);
+		return row;	
 	}
 
 	public boolean isValidPosition(int cellId) {
 		boolean isValid = true;
-
 		if(numQueens == 8) throw new GameOverException();	// just return out of the method
 
 		// the game is still on, so let us continue...
@@ -24,11 +23,62 @@ public class GameStrategy {
 		int col = getColumn(cellId);
 		
 		int row = getRow(cellId);
+			
+		//is there a queen in the same row
+		for(int i=0;i<8;i++) {
+			if(placedQueens[row][i]==true) {
+				return false;
+			}
+		}
+		//is there a queen in the same col
+		for(int i=0;i<8;i++) {
+			if(placedQueens[i][col]==true) {
+				return false;
+			}
+		}
+		//is there a queen on the first diagonal
+		int x=row+1;
+		int y=col+1;
+		while(x<8&&y<8) {
+			if(placedQueens[x][y]==true) {
+				return false;
+			}
+			x++;
+			y++;
+		}
 		
-		/*
-			WRITE YOUR LOGIC HERE...............................
-
-		*/
+		x=row-1;
+		y=col-1;
+		while(x>=0&&y>=0) {
+			if(placedQueens[x][y]==true) {
+				return false;
+			}
+			x--;
+			y--;
+		}
+		//is there a queen in the second diagonal
+		x=row+1;
+		y=col-1;
+		while(x<8&&y>=0) {
+			if(placedQueens[x][y]==true) {
+				return false;
+			}
+			x++;
+			y--;
+		}
+		x=row-1;
+		y=col+1;
+		while(x>=0&&y<8) {
+			if(placedQueens[x][y]==true) {
+				return false;
+			}
+			x--;
+			y++;
+		}
+		if(isValid) {
+			placedQueens[row][col]=true;
+			numQueens++;
+		}
 		return isValid;
 	}
 }
